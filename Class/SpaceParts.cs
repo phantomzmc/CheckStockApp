aee040b1682ff7e7c1878b7035a641b3873df3f8
Thunnathorn <phantomzmc@gmail.com>
2019-08-05 16:18:32 +0700

    first commit

---
+++ Class/SpaceParts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckStockApp
+{
+    public static class SpacePartsList
+    {
+        private static List<SpacePart> listSpaceParts { get; set; }
+
+        static SpacePartsList()
+        {
+            var temp = new List<SpacePart>();
+
+            //AddBooth(temp);
+            //AddBooth(temp);
+            ////AddBooth(temp);
+
+            //Booths = temp.OrderBy(i => i.PlateBooth).ToList();
+        }
+        public class SpacePart
+        {
+            public String ID_Item { get; set; }
+            public String Name_Item { get; set; }
+            public String Group_Item { get; set; }
+            public double Sell_Price_Unit { get; set; }
+            public double Sell_Price_All { get; set; }
+            public double Cost_Price_Unit { get; set; }
+            public double Cost_Price_All { get; set; }
+            public String Self_Main { get; set; }
+            public String Self_Try { get; set; }
+            public DateTime Date_Count_Stock { get; set; }
+            public double Total_Stock { get; set; }
+            public double Amound_Sold { get; set; }
+            public double Number_Parts_Booking { get; set; }
+            public double Inventory_Last_Month { get; set; }
+
+            public SpacePart()
+            {
+                this.ID_Item = null;
+                this.Name_Item = null;
+                this.Group_Item = null;
+                this.Sell_Price_Unit = 0.0;
+                this.Sell_Price_All = 0.0;
+                this.Cost_Price_Unit = 0.0;
+                this.Cost_Price_All = 0.0;
+                this.Self_Main = null;
+                this.Self_Try = null;
+                this.Date_Count_Stock = DateTime.MinValue;
+                this.Total_Stock = 0.0;
+                this.Amound_Sold = 0.0;
+                this.Number_Parts_Booking = 0.0;
+                this.Inventory_Last_Month = 0.0;
+            }
+
+        }
+    }
+
+}
---
+++ WCFHttpService.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.ServiceModel;
+using Android.Net;
+using Java.Lang;
+
+namespace AndroidForBC
+{
+    public class WCFHttpService
+    {
+        private static string _server = "http://192.168.1.195:8382/Service.svc";
+
+
+
+        public static BasicHttpBinding CreateBasicHttp()
+        {
+
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                Name = "basicHttpBinding",
+                MaxBufferSize = 2147483647,
+                MaxReceivedMessageSize = 2147483647
+            };
+            TimeSpan timeout = new TimeSpan(0, 10, 30);
+            binding.SendTimeout = timeout;
+            binding.OpenTimeout = timeout;
+            binding.ReceiveTimeout = timeout;
+            return binding;
+        }
+
+        public static EndpointAddress GetEndPoint(string serviceUrl)
+        {
+            return new EndpointAddress(serviceUrl);
+        }
+
+        public static ServiceClient GetService1Client()
+        {
+            BasicHttpBinding binding = WCFHttpService.CreateBasicHttp();
+            return new ServiceClient(binding, WCFHttpService.GetEndPoint(_server));
+        }
+
+
+        //public static Service1Client GetService1Client(Context c)
+        //{
+        //    BasicHttpBinding binding = WCFHttpService.CreateBasicHttp();
+        //     return new Service1Client(binding, WCFHttpService.GetEndPoint("http://asiakaset.dyndns.biz:8085/Service1.svc"));
+        //    //return new Service1Client(binding, WCFHttpService.GetEndPoint("http://192.168.1.188:8080/Service1.svc"));
+        //    //return new Service1Client(binding, WCFHttpService.GetEndPoint("http://45ab0279e86e.sn.mynetname.net:50000/Service1.svc"));
+        //}
+
+
+        public static NetworkInfo CheckInternet(Context c)
+        {
+            
+            ConnectivityManager connectivityManager = (ConnectivityManager)c.GetSystemService(Context.ConnectivityService);
+            NetworkInfo networkInfo = connectivityManager.ActiveNetworkInfo;
+            //if (networkInfo != null)
+            //{
+            //    connect = true;
+            //}
+            return networkInfo;
+        }
+    }
+}
